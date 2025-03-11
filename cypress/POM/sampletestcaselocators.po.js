4f01dbbea044cb2c00593978936929fc20c782ef
Nagesh73308 <nageshmahesh111@gmail.com>
2025-03-11 11:12:44 +0530

    11/03/2025commits

--- cypress/POM/sampletestcaselocators.po.js
+++ cypress/POM/sampletestcaselocators.po.js
@@ -20,6 +20,69 @@ class signinandlogin{
     loginbuttonlocator(){
         return 'button[data-qa="login-button"]'
     }
+    deleteaccountbutton(){
+        return ' Delete Account'
+    }
+    continuebutton(){
+        return 'a[data-qa="continue-button"]'
+    }
+    logoutbutton(){
+        return 'a[href="/logout"]'
+    }
+    productbutton(){
+        return 'a[href="/products'
+    }
+    subscriptionbutton(){
+        return '#susbscribe_email'
+    }
+    cartbutton(){
+        return ' Cart'
+    }
+    product1addtocartbutton(){
+        return 'a[data-product-id="1"]'
+    }
+    product2addtocartbutton(){
+        return 'a[data-product-id="2"]'
+    }
+    continueshopingbutton(){
+        return 'button[data-dismiss="modal"]'
+    }
+    viewcartbutton(){
+        return 'View Cart'
+    }
+    searchproductbutton(){
+        return 'input[placeholder="Search Product"]'
+    }
+    contactusbutton(){
+        return 'a[href="/contact_us"]'
+    }
+    viewproductbutton(){
+        return 'a[href="/product_details/1"]'
+    }
+    clicksearchbutton(){
+        return 'button[class="btn btn-default btn-lg"]'
+    }
+    fileuplodebutton(){
+        return 'input[type="file"]'
+    }
+    contactformsubmitbbutton(){
+        return 'input[type="submit"]'
+    }
+    contactformemaillocator(){
+        return 'input[placeholder="Email"]'
+    }
+    contactformsubjectlocator(){
+        return 'input[placeholder="Subject"]'
+    }
+    contactformnamelocator(){
+        return 'input[placeholder="Name"]'
+    }
+    contactformmassagelocator(){
+        return '#message'
+    }
+    clickonsubscribelocator(){
+        return 'i[class="fa fa-arrow-circle-o-right"]'
+    }
 
 }
 
